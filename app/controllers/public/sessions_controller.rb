6cb32b2e7b78e0cbcc6498710f4d26e12dc5b972
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-10-09 12:56:01 +0000

    admin名前ログイン

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_in_params, only: [:create]
   def after_sign_in_path_for(resource)
     posts_path
+  end
+  
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
   end
   # GET /resource/sign_in
   # def new
