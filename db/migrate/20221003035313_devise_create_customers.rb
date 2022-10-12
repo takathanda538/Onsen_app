bcc124a56ad5b1c544c5cfc6219b5e1074606ff8
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-10-12 11:57:07 +0000

    bbbbb

--- db/migrate/20221003035313_devise_create_customers.rb
+++ db/migrate/20221003035313_devise_create_customers.rb
@@ -4,6 +4,8 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       ## Database authenticatable
+      t.string :name,              null: false, default: ""
+      t.string :introduce
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
