b3f952fe8786aaa972ef793a8cfe88c3d58ab0ff
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-10-12 11:11:33 +0000

    aa

--- db/migrate/20221003051113_create_likes.rb
+++ db/migrate/20221003051113_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.references :customer, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :customer,type: :bigint, null: false, foreign_key: true
+      t.references :post,type: :bigint, null: false, foreign_key: true
 
       t.timestamps
     end
